5af002f0522fdd699ebe50bc6e0383db1206f5eb
Anton Lashchotka <a.lashchotka@knomary.com>
2024-08-30 00:20:36 +0300

    Navigation init

---
+++ src/navigation/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Main from '../screens/Main';
+import Registration from '../screens/Registration';
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Registration" component={Registration} />
+        <Stack.Screen name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
