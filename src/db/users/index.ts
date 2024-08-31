ceee740a24d70f2fd289b4186053b0dae6785f8f
Anton Lashchotka <a.lashchotka@knomary.com>
2024-08-31 18:21:27 +0300

    feature/Registration-Logic: add get user methods for Users table

--- src/db/users/index.ts
+++ src/db/users/index.ts
@@ -2,7 +2,7 @@ import { connectToDatabase } from '..';
 
 import { User } from './types';
 
-export const createTable = async () => {
+export const createTable = async (): Promise<void> => {
   try {
     const db = await connectToDatabase();
 
@@ -21,18 +21,77 @@ export const createTable = async () => {
   }
 };
 
-export const createUser = async (email: User['email'], username: User['username']) => {
+export const getUserById = async (id: User['id']): Promise<User> => {
   try {
     const db = await connectToDatabase();
 
-    const insertQuery = `
+    const query = `
+      SELECT * FROM Users WHERE id = ?
+    `;
+
+    const values = [id];
+
+    const data = await db.executeSql(query, values);
+
+    return data[0].rows.item(0);
+  } catch (error) {
+    console.error(error);
+    throw Error('Failed to get user by id from database (Users table)');
+  }
+};
+
+export const getUserByEmail = async (email: User['email']): Promise<User> => {
+  try {
+    const db = await connectToDatabase();
+
+    const query = `
+      SELECT * FROM Users WHERE email = ?
+    `;
+
+    const values = [email];
+
+    const data = await db.executeSql(query, values);
+
+    return data[0].rows.item(0);
+  } catch (error) {
+    console.error(error);
+    throw Error('Failed to get user by email from database (Users table)');
+  }
+};
+
+export const getUserByUsername = async (username: User['username']): Promise<User> => {
+  try {
+    const db = await connectToDatabase();
+
+    const query = `
+      SELECT * FROM Users WHERE username = ?
+    `;
+
+    const values = [username];
+
+    const data = await db.executeSql(query, values);
+
+    return data[0].rows.item(0);
+  } catch (error) {
+    console.error(error);
+    throw Error('Failed to get user by username from database (Users table)');
+  }
+};
+
+export const createUser = async (email: User['email'], username: User['username']): Promise<User> => {
+  try {
+    const db = await connectToDatabase();
+
+    const query = `
       INSERT INTO Users (email, username)
       VALUES (?, ?)
     `;
 
     const values = [email, username];
 
-    return db.executeSql(insertQuery, values);
+    const data = await db.executeSql(query, values);
+
+    return data[0].rows.item(0);
   } catch (error) {
     console.error(error);
     throw Error('Failed to create user in database (Users table)');
