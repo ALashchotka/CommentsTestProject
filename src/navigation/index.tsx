c40d7007b999525972bf64c77903c884df62e7b0
Anton Lashchotka <a.lashchotka@knomary.com>
2024-09-02 00:08:43 +0300

    feature/Comments-UI: temporary add default header for Main screen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -12,10 +12,11 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator>
         <Stack.Screen
           name={REGISTRATION_SCREEN_NAME}
           component={Registration}
+          options={{ headerShown: false }}
         />
         <Stack.Screen
           name={MAIN_SCREEN_NAME}
