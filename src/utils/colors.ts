2731d7d1a260310cbcb9f4cdcb8a6d886b483bc8
Anton Lashchotka <a.lashchotka@knomary.com>
2024-09-02 01:24:31 +0300

    feature/Comments-UI: update Avatar colors

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,4 +1,4 @@
-const COLORS = ['#485DC8', '#3EBFC6', '#FFC207', '#FF3E3B', '#00C958', '#387EFF'];
+const COLORS = ['#A0C4FF', '#B9FBC0', '#FFE156', '#FF677D', '#F5B4A2', '#C3B5E8'];
 
 export function stringToColor(str: string) {
   return str ? COLORS[str.split('').reduce((acc, item) => acc + item.charCodeAt(0), 0) % COLORS.length] : '#C4C4C4';
